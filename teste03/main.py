b812148f59a00fe99ec1a1543ea1ef3176193d53
Paulo Henrique <ph55@hotmail.com>
2023-03-05 13:19:20 -0300

    Update teste04

--- teste03/main.py
+++ teste03/main.py
@@ -3,7 +3,7 @@ import json
 with open('dados.json') as faturamento_mensal:
     dados_faturamento = json.load(faturamento_mensal)
 
-'Função que armazena os valores diários faturados dentro de um array/list e  retira os dias não faturados'
+#Função que armazena os valores diários faturados dentro de um array/list e  retira os dias não faturados
 def converte_dados():
     lista_ganhos = []
     for x in dados_faturamento:
@@ -12,13 +12,13 @@ def converte_dados():
     return lista_ganhos
 
 
-'Função que verifica o dia de menor faturamento'
+#Função que verifica o dia de menor faturamento
 def menor_valor_faturado():
     menor_valor = min(converte_dados)
     print(f'\nO MENOR valor faturado no mês foi de {menor_valor:.2f}.')
 
 
-'Função que verifica o dia de maior faturamento. OBS: PODERIA USAR A FUNÇÃO MAX(), MAIS OBTEI POR UTILIZAR  A LÓGICA'
+#Função que verifica o dia de maior faturamento. OBS: PODERIA USAR A FUNÇÃO MAX(), MAIS OBTEI POR UTILIZAR  A LÓGICA
 def maior_valor_faturado():
     maior_valor = 0
     for x in converte_dados():
@@ -27,7 +27,7 @@ def maior_valor_faturado():
     print(f'\nO MAIOR valor faturado no mês foi de R${maior_valor:.2f}.')
 
 
-'Calcula a média mensal de faturamento do mês'
+#Calcula a média mensal de faturamento do mês
 def calcula_media_mensal():
     lucro_mensal = 0
     for x in converte_dados():
@@ -35,7 +35,7 @@ def calcula_media_mensal():
     media_mensal = lucro_mensal / len(converte_dados())
     return media_mensal
 
-'Calcula a quantidade de dias que o lucro diário superou a média mensal'
+#Calcula a quantidade de dias que o lucro diário superou a média mensal
 def calcula_dias_superados():
     dias_superados = 0
     for x in converte_dados():
---
+++ teste04/main.py
@@ -0,0 +1,34 @@
+regional_values = {"SP": 67836.43, "RJ": 36678.66, "MG": 29229.88, "ES": 27165.48, "OUTROS": 19849.53}
+
+
+# Calculado o total mensal de cada estado
+def calculate_monthly_total():
+    amount_list = []
+    for x in regional_values.values():
+        amount_list.append(x)
+    amount_total = sum(amount_list)
+    return amount_total
+
+
+# Calcula o percentual de cada estado
+def calculate_percentage():
+    percentage_list = []
+    for x in regional_values.values():
+        percentage = (x / calculate_monthly_total()) * 100
+        percentage_list.append(percentage)
+    return percentage_list
+
+
+# Exibi percentual de cada estado
+def display_list():
+    states = []
+    count = 0
+    for x in regional_values.keys():
+        states.append(x)
+    print(F'FATURAMENTO TOTAL -> {calculate_monthly_total()}')
+    for i in calculate_percentage():
+        count += 1
+        print(f'{states[count - 1]} -> {i:.2f}%')
+
+
+display_list()
